be09c8d56bee3047444160e97cfb4d2e9a9f6045
Emran Younas <emranyounasdxb@gmail.com>
2026-07-02 17:45:29 +0400

    Add company contact details

---
+++ lib/company-info.ts
@@ -0,0 +1,12 @@
+export const companyInfo = {
+  supportEmail: 'info@edrivedubai.ae',
+  bookingEmail: 'booking@edrivedubai.ae',
+  landlineDisplay: '+971 4 611 3100',
+  landlineHref: '+97146113100',
+  whatsappDisplay: '+971 56 416 5922',
+  whatsappHref: '971564165922',
+  locationName: 'Dubai Island Marina',
+  mapEmbedSrc: 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d4842.7767339016555!2d55.2880943753848!3d25.290003877651273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDE3JzI0LjAiTiA1NcKwMTcnMjYuNCJF!5e1!3m2!1sen!2sae!4v1782999814491!5m2!1sen!2sae'
+};
+
+export const whatsappUrl = `https://wa.me/${companyInfo.whatsappHref}`;
