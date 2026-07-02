068a85422f43c440fffc54dadba46a4fd3facc61
Emran Younas <emranyounasdxb@gmail.com>
2026-07-02 15:13:04 +0400

    Refine premium card surfaces

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn('min-w-0 rounded-lg border border-border bg-card text-card-foreground shadow-glass', className)} {...props} />
+  <div ref={ref} className={cn('premium-surface min-w-0 rounded-[2rem] text-card-foreground', className)} {...props} />
 ));
 Card.displayName = 'Card';
 
 const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn('flex min-w-0 flex-col gap-1.5 p-5', className)} {...props} />
+  <div ref={ref} className={cn('flex min-w-0 flex-col gap-1.5 p-6', className)} {...props} />
 ));
 CardHeader.displayName = 'CardHeader';
 
@@ -22,12 +22,12 @@ const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttribu
 CardDescription.displayName = 'CardDescription';
 
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn('min-w-0 p-5 pt-0', className)} {...props} />
+  <div ref={ref} className={cn('min-w-0 p-6 pt-0', className)} {...props} />
 ));
 CardContent.displayName = 'CardContent';
 
 const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn('flex items-center p-5 pt-0', className)} {...props} />
+  <div ref={ref} className={cn('flex items-center p-6 pt-0', className)} {...props} />
 ));
 CardFooter.displayName = 'CardFooter';
 
