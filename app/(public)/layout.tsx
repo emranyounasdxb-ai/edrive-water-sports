3a4ca23e74be0f32d401044878169e687e45dd04
Emran Younas <emranyounasdxb@gmail.com>
2026-07-07 13:32:58 +0400

    Remove header action script from public layout

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from 'react';
-import { HeaderActionRefiner } from '@/components/edrive/header-action-refiner';
 import { PublicShell } from '@/components/edrive/public-shell';
 
 export default function PublicLayout({ children }: { children: ReactNode }) {
-  return <PublicShell><HeaderActionRefiner />{children}</PublicShell>;
+  return <PublicShell>{children}</PublicShell>;
 }
