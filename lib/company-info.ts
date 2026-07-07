40bf34e39864753399c52ebea32128e41d0dda5d
Emran Younas <emranyounasdxb@gmail.com>
2026-07-07 12:22:24 +0400

    Correct public location wording

--- lib/company-info.ts
+++ lib/company-info.ts
@@ -5,9 +5,9 @@ export const companyInfo = {
   landlineHref: '+97146113100',
   whatsappDisplay: '+971 56 416 5922',
   whatsappHref: '971564165922',
-  locationName: 'Dubai Island Marina',
+  locationName: 'Dubai Islands',
   locationAddress: 'Dubai Islands, Dubai, United Arab Emirates',
-  mapLink: 'https://www.google.com/maps/search/?api=1&query=Dubai%20Island%20Marina',
+  mapLink: 'https://www.google.com/maps/search/?api=1&query=Dubai%20Islands%2C%20Dubai%2C%20United%20Arab%20Emirates',
   mapEmbedSrc: 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d4842.7767339016555!2d55.2880943753848!3d25.290003877651273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDE3JzI0LjAiTiA1NcKwMTcnMjYuNCJF!5e1!3m2!1sen!2sae!4v1782999814491!5m2!1sen!2sae'
 };
 
