5ff6a86ffdb8492a0907d0e69a111d7719dd64b4
Emran Younas <emranyounasdxb@gmail.com>
2026-07-13 19:53:43 +0400

    Stabilize Supabase query result typing

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -35,8 +35,40 @@ const client = createClient(supabaseUrl, supabaseAnonKey, {
   }
 });
 
+type QueryError = { message: string } | null;
+type QueryResult<T> = { data: T | null; error: QueryError; count?: number | null };
+
+type FlexibleQuery<T = any[]> = Promise<QueryResult<T>> & {
+  select: (...args: any[]) => FlexibleQuery<T>;
+  insert: (...args: any[]) => FlexibleQuery<any[]>;
+  update: (...args: any[]) => FlexibleQuery<any[]>;
+  upsert: (...args: any[]) => FlexibleQuery<any[]>;
+  delete: (...args: any[]) => FlexibleQuery<any[]>;
+  order: (...args: any[]) => FlexibleQuery<T>;
+  limit: (...args: any[]) => FlexibleQuery<T>;
+  range: (...args: any[]) => FlexibleQuery<T>;
+  eq: (...args: any[]) => FlexibleQuery<T>;
+  neq: (...args: any[]) => FlexibleQuery<T>;
+  gt: (...args: any[]) => FlexibleQuery<T>;
+  gte: (...args: any[]) => FlexibleQuery<T>;
+  lt: (...args: any[]) => FlexibleQuery<T>;
+  lte: (...args: any[]) => FlexibleQuery<T>;
+  like: (...args: any[]) => FlexibleQuery<T>;
+  ilike: (...args: any[]) => FlexibleQuery<T>;
+  is: (...args: any[]) => FlexibleQuery<T>;
+  in: (...args: any[]) => FlexibleQuery<T>;
+  contains: (...args: any[]) => FlexibleQuery<T>;
+  containedBy: (...args: any[]) => FlexibleQuery<T>;
+  match: (...args: any[]) => FlexibleQuery<T>;
+  not: (...args: any[]) => FlexibleQuery<T>;
+  or: (...args: any[]) => FlexibleQuery<T>;
+  filter: (...args: any[]) => FlexibleQuery<T>;
+  single: () => FlexibleQuery<any>;
+  maybeSingle: () => FlexibleQuery<any>;
+};
+
 type FlexibleSupabaseClient = Omit<typeof client, 'from'> & {
-  from: (...args: Parameters<typeof client.from>) => any;
+  from: (...args: Parameters<typeof client.from>) => FlexibleQuery;
 };
 
 export const supabase = client as FlexibleSupabaseClient;
